4930ed3310d4839a98aac0c10bd61c8d8aeb7295
unknown <rndaerio@gmail.com>
2026-05-29 22:49:52 +0700

    fix: serve React SPA on root route instead of Laravel welcome page

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,7 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('app');
 });
 
 // Serve the React SPA for all auth-related frontend routes
