3474a43f703b79534f2d782c3f4dd422b6582bd6
unknown <rndaerio@gmail.com>
2026-06-01 23:02:39 +0700

    refactor: restructure frontend architecture

---
+++ src/webapp/src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import LandingPage from './features/landing/LandingPage/LandingPage';
+import Login from './features/auth/Login/Login';
+import Register from './features/auth/Register/Register';
+
+const App = () => {
+    return (
+        <Routes>
+            <Route path="/" element={<LandingPage />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+        </Routes>
+    );
+};
+
+export default App;
