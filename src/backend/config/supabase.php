3ebfbf54110e83a76fdba57bc5c780a81c2fcc1f
Muhamad Afif <muhamadafifajiputra@gmail.com>
2026-06-09 23:22:45 +0700

    feat: update skema penyimpanan data image

---
+++ src/backend/config/supabase.php
@@ -0,0 +1,8 @@
+<?php
+
+return [
+    'url' => env('SUPABASE_URL'),
+    'key' => env('SUPABASE_KEY'),
+    'service_role' => env('SUPABASE_SERVICE_ROLE'),
+    'bucket' => env('SUPABASE_BUCKET', 'mitra-assets'),
+];
